8d34056aea4508e1c764e18d5da641336035686e
mohaiminul islam <121026585+mohaiminulsuvo@users.noreply.github.com>
2023-09-15 23:49:56 +0300

    Create Pizza_order_system.py

---
+++ Pizza_order_system.py
@@ -0,0 +1,23 @@
+print("Welcome to Pizza delivery portal")
+bill = 0
+size = input("What size of pizza do you want? S, M, L    ")
+add_pepperoni = input('Do you want pepperoni? Y or N   ')
+extra_cheese = input('Do you want extra cheese ? Y or N   ')
+bill = 0
+
+if size == "S":
+  bill += 15
+elif size == "M":
+  bill += 20
+else:
+  bill += 25
+  
+if add_pepperoni == "Y":
+    if size == "S":
+      bill += 2
+    else:
+      bill += 3
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"the final bill will be $ {bill}")
